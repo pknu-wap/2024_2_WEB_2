5d8f6cdc04ab3f570d5ae4a492a9329dd3fa1767
SHSong <99pyues2@gmail.com>
2024-10-08 03:57:19 +0900

    feature : CORS설정 및 소셜로그인 구현

---
+++ server/src/main/java/wap/web2/server/payload/AuthResponse.java
@@ -0,0 +1,14 @@
+package wap.web2.server.payload;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class AuthResponse {
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+    public AuthResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}

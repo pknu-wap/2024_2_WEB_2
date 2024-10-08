d4f22dd4c541903bc418c73c14c02b7d7d8cc770
SHSong <99pyues2@gmail.com>
2024-10-08 18:37:55 +0900

    featre : Project, Image엔티티

---
+++ server/src/main/java/wap/web2/server/domain/Project.java
@@ -0,0 +1,41 @@
+package wap.web2.server.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long projectId;
+
+    private String title;
+
+    private String projectType;
+
+    @Lob
+    @Column(length = 9000) // 글 내용은 길이 9000 (한글 기준 3000자)
+    private String content;
+
+    private String summary;
+
+    private Long semester;
+
+    private Long vote;
+
+    private Long projectYear;
+
+    private String thumbnail;
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Image> images = new ArrayList<>();
+}

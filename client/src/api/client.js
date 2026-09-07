54df294e57fe67480c05e7cb2d186f3342ff62f0
psvm203 <psvm203@proton.me>
2026-09-07 09:54:18 +0900

    chore: 프론트엔드에서 하드코딩된 API 주소 삭제

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "https://waps-api.duckdns.org",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",

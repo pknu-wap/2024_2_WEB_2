8b3622aa841246297d4054c103fb703eaa7cb560
Yeram <rlaexram@pukyong.ac.kr>
2026-09-08 14:51:26 +0900

    feat: 메뉴 페이지 UI 수정

--- client/src/components/MainLayout.js
+++ client/src/components/MainLayout.js
@@ -1,9 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useMatch } from "react-router-dom";
 import "../App.css"; // .container 스타일을 가져오기 위해 임포트
 
 const MainLayout = () => {
+  const isSplashPage = useMatch("/");
+  const isProjectPage = useMatch("/ProjectPage");
+  const hidePageShadow = isSplashPage || isProjectPage;
+
   return (
-    <div className="container">
+    <div className={`container${hidePageShadow ? " container--no-shadow" : ""}`}>
       <Outlet />{" "}
       {/* 이 컴포넌트를 부모로 하는 자식 라우트들이 여기에 렌더링됨 */}
     </div>
